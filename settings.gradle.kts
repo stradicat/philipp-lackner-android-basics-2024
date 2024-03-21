556424edd55a7663ff45c7ca5b6c934c62aa1a5f
Dennis Mayr <dmayr.dev@gmail.com>
2024-03-20 21:47:47 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Phillip Lackner - Android Basics 2024"
+include(":app")
