9d6a5cf7aec3c5304e0040814cf5a76cfe30b2fb
Dennis Mayr <dmayr.dev@gmail.com>
2024-03-20 21:52:58 -0300

    Moved 'viewModel' to narrower scope, importing dependency 'lifecycle-viewmodel-compose', returning Factory object.

--- app/src/main/java/dev/dmayr/philliplackner_androidbasics2024/MainActivity.kt
+++ app/src/main/java/dev/dmayr/philliplackner_androidbasics2024/MainActivity.kt
@@ -3,22 +3,23 @@ package dev.dmayr.philliplackner_androidbasics2024
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.dmayr.philliplackner_androidbasics2024.ui.theme.PhillipLacknerAndroidBasics2024Theme
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel by viewModels<ContactsViewModel>() // instance of ViewModel to preserve states; it will outlive the Activity lifecycle.
+//    private val viewModel by viewModels<ContactsViewModel>() // instance of ViewModel to preserve states; it will outlive the Activity lifecycle.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +29,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             PhillipLacknerAndroidBasics2024Theme {
                 // A surface container using the 'background' color from the theme
+
+                val viewModel = viewModel<ContactsViewModel>(
+                    factory = object : ViewModelProvider.Factory {
+                        @Suppress("UNCHECKED_CAST")
+                        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                            return ContactsViewModel(
+                                helloWorld = "Hello, World!"
+                            ) as T
+                        }
+                    }
+                )
                 Surface(
                     modifier = Modifier.fillMaxSize(),
 //                    color = MaterialTheme.colorScheme.background
