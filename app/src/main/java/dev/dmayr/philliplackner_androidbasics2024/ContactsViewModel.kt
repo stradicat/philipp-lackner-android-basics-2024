657fa73105c5abb8744c4c1b599dd5777f56a817
Dennis Mayr <dmayr.dev@gmail.com>
2024-04-23 21:03:38 -0400

    ViewModel: storing state of background color, and triggering state change

--- app/src/main/java/dev/dmayr/philliplackner_androidbasics2024/ContactsViewModel.kt
+++ app/src/main/java/dev/dmayr/philliplackner_androidbasics2024/ContactsViewModel.kt
@@ -1,6 +1,5 @@
 package dev.dmayr.philliplackner_androidbasics2024
 
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
