f4bf8291dcedb2b873973b793d6d0a079551f20b
Dennis Mayr <dmayr.dev@gmail.com>
2024-05-19 18:10:52 -0400

    ViewModel: scoped implementation, returns a ViewModel-typed object

--- app/src/main/java/dev/dmayr/philliplackner_androidbasics2024/MainActivity.kt
+++ app/src/main/java/dev/dmayr/philliplackner_androidbasics2024/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,14 +15,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.dmayr.philliplackner_androidbasics2024.ui.theme.PhillipLacknerAndroidBasics2024Theme
 
 class MainActivity : ComponentActivity() {
 
     private fun getTAG(): String = "MainActivity"
 
-    private val viewModel by viewModels<ContactsViewModel>() // instance of ViewModel to preserve states; it will outlive the Activity lifecycle.
-//    private val viewModel = ViewModelProvider(this).get(ContactsViewModel::class.java)
+//    private val viewModel by viewModels<ContactsViewModel>() // general instance of ViewModel to preserve states; it will outlive the Activity lifecycle.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,16 +35,17 @@ class MainActivity : ComponentActivity() {
             PhillipLacknerAndroidBasics2024Theme {
                 // A surface container using the 'background' color from the theme
 
-//                // Jetpack Compose way:
-//                val viewModel =
-//                    viewModel<ContactsViewModel>(factory = object : ViewModelProvider.Factory {
-//                        @Suppress("UNCHECKED_CAST")
-//                        override fun <T : ViewModel> create(modelClass: Class<T>): T {
-//                            return ContactsViewModel(
-//                                helloWorld = "Hello, World!"
-//                            ) as T
-//                        }
-//                    })
+                // Jetpack Compose way:
+                @Suppress("UNCHECKED_CAST") val viewModel = viewModel<ContactsViewModel>(
+                    factory = object : ViewModelProvider.Factory {
+                        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+//                            return super.create(modelClass)
+                            return ContactsViewModel() as T
+                            // inside ContactsViewModel() we can pass the class's constructor arguments if there were any
+                        }
+                    }
+                )
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = viewModel.backgroundColor
