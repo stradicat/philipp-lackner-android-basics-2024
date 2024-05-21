e4c6b23326d61781e22f98b9c858968d86298bb3
Dennis Mayr <dmayr.dev@gmail.com>
2024-05-20 21:36:09 -0400

    05.- Resources & Qualifiers: showing added bitmap on view

--- app/src/main/java/dev/dmayr/philliplackner_androidbasics2024/MainActivity.kt
+++ app/src/main/java/dev/dmayr/philliplackner_androidbasics2024/MainActivity.kt
@@ -1,26 +1,21 @@
 package dev.dmayr.philliplackner_androidbasics2024
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import dev.dmayr.philliplackner_androidbasics2024.ui.theme.PhillipLacknerAndroidBasics2024Theme
 
-// 2.- passing Context allows the class to interact with Android OS components
-class MyClass(private val context: Context)
-
 class MainActivity : ComponentActivity() {
 
-    // 4.- Let's create an instance of a ViewModel
-    private val myViewModel by viewModels<MyViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,13 +23,7 @@ class MainActivity : ComponentActivity() {
         // 1.- Context: middleman between an app and Android OS components
         // e.g. database, filesystem, camera, providers in general
 
-//        val myContext: Context = this
-//        // 'this' refers to this instance of MainActivity
-        // 3.- Each Context has a specific lifetime, depending on where it was declared.
-//        myViewModel.context = this
-
-//        4.- Calling the Context on the current activity:
-        this.applicationContext
+        // 2.- Each Context has a specific lifetime, depending on where it was declared.
 
         setContent {
             PhillipLacknerAndroidBasics2024Theme {
@@ -45,6 +34,10 @@ class MainActivity : ComponentActivity() {
                     Greeting("This is an Android app")
 
                 }
+
+                Image(
+                    painter = painterResource(id = R.drawable.cheetara_mirando),
+                    contentDescription = "Cheetara mirando")
             }
         }
     }
